7904e8cb42812fa9b5a1aa51ddc6ae9aa91130b3
Tiago Edgar Rasia <tiago_rasia@hotmail.com>
2023-07-14 18:21:05 -0300

    Update ProfissaoRepository.java

--- Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/ProfissaoRepository.java
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/ProfissaoRepository.java
@@ -2,6 +2,11 @@ package com.mjvinnovation.school.jobby.repository;
 
 import com.mjvinnovation.school.jobby.model.Profissao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProfissaoRepository  extends JpaRepository<Profissao, Integer> {
+
+    @Query("SELECT p FROM Profissao p WHERE p.nome LIKE :nome")
+    Profissao findByNome(String nome);
+
 }
