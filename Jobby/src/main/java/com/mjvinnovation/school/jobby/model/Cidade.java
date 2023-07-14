162c6bc0cae02c0ed28f87977bc82b88d70053ea
Rasia83 <tiago_rasia@hotmail.com>
2023-07-14 14:22:06 -0300

    Update Cidade.java

--- Jobby/src/main/java/com/mjvinnovation/school/jobby/model/Cidade.java
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/model/Cidade.java
@@ -1,22 +1,30 @@
 package com.mjvinnovation.school.jobby.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
-@Data
+//@Data
+@Getter
+@EqualsAndHashCode
+@ToString
 @Entity
 @Table(name = "cidades")
 public class Cidade {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigoMunicipio;
 
-    @Column(nullable = false, length = 50)
-    private String nome;
-/*
-    @ManyToOne
-    @JoinColumn(name="id_cadastro")
-    private Cadastro cadastro;
-*/
+    // @Column(nullable = false, length = 50)
+    private String nomeMunicipio;
+
+    // @Column(length = 2 )
+    private String uf;
+
+    // @Column(length = 20) // nullable = false,
+    private String nomeUf;
+
 }
