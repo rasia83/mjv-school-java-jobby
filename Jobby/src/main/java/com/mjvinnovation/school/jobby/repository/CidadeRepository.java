bbb2523697123004e2f4d768f488af24ea6d0d86
Tiago Edgar Rasia <tiago_rasia@hotmail.com>
2023-07-14 18:20:54 -0300

    Update CidadeRepository.java

--- Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/CidadeRepository.java
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/CidadeRepository.java
@@ -2,6 +2,11 @@ package com.mjvinnovation.school.jobby.repository;
 
 import com.mjvinnovation.school.jobby.model.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+    @Query("SELECT c FROM Cidade c WHERE c.uf = :uf AND c.nomeMunicipio = :nomeMunicipio")
+    Cidade findByUfAndNome(String uf, String nomeMunicipio);
+
 }
