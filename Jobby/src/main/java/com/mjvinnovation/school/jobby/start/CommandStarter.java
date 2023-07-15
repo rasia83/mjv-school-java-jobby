311941c6a145a1a144060cd8e1d1dd751339a53d
Rasia83 <tiago_rasia@hotmail.com>
2023-07-15 05:55:55 -0300

    Update CommandStarter.java

--- Jobby/src/main/java/com/mjvinnovation/school/jobby/start/CommandStarter.java
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/start/CommandStarter.java
@@ -24,8 +24,8 @@ public class CommandStarter implements CommandLineRunner {
     @Autowired
     private CidadeRepository cidadeRepository;
 
-    @Autowired
-    private EnderecoRepository enderecoRepository;
+    //@Autowired
+    //private EnderecoRepository enderecoRepository;
 
     @Autowired
     private ProfissaoRepository profissaoRepository;
@@ -36,9 +36,20 @@ public class CommandStarter implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Use este recurso caso queira executar algum comando");
 
+        /* List<Cidade>  cidades = cidadeRepository.findByUfOrderByNomeMunicipio("RS");
+        for (Cidade c:cidades) {
+            System.out.println(c.getNomeMunicipio());
+        }*/
+
         Cadastro cadastro = preencherCadastro();
 
         cadastroRepository.save(cadastro);
+
+        CadastroExperiencia cadastroExperiencia = preencherCadastroExperiencia(cadastro);
+
+        cadastroExperienciaRepository.save(cadastroExperiencia);
+
+        cadastroRepository.deleteById(1);
 
     }
 
@@ -57,7 +68,7 @@ public class CommandStarter implements CommandLineRunner {
         endereco.setBairro("centro");
         endereco.setCep(13800000L);
         // Cidade cidade = cidadeRepository.findById(3550308).get(); // São Pualo SP
-        Cidade cidade = cidadeRepository.findByUfAndNome("SP", "São Paulo");
+        Cidade cidade = cidadeRepository.findByUfAndNomeMunicipio("SP", "São Paulo");
         endereco.setCidade(cidade);
         cadastro.setEndereco(endereco);
 
@@ -72,69 +83,47 @@ public class CommandStarter implements CommandLineRunner {
         cadastro.setPretencaoSalarial(pretencaoSalarial);
 
         List<String> habilidades = new ArrayList<>();
-        // Java Core Skills
-        habilidades.add("Java programming language");
-        habilidades.add("Object-oriented programming (OOP)");
-        habilidades.add("Java Collections framework");
-        habilidades.add("Exception handling");
-        habilidades.add("Multithreading and concurrency");
-        habilidades.add("IO and File handling");
-        habilidades.add("Java Generics");
-        habilidades.add("Lambda expressions");
-        habilidades.add("Java Streams API");
-        // Java Enterprise Edition (Java EE)
-        habilidades.add("Java Servlets");
-        habilidades.add("JavaServer Pages (JSP)");
-        habilidades.add("JavaServer Faces (JSF)");
-        habilidades.add("Enterprise JavaBeans (EJB)");
+        habilidades.add("Java");
         habilidades.add("Java Persistence API (JPA)");
-        habilidades.add("Java Message Service (JMS)");
-        habilidades.add("Java Transaction API (JTA)");
-        habilidades.add("Java Authentication and Authorization Service (JAAS)");
-        // Frameworks and Libraries
-        habilidades.add("Spring Framework");
         habilidades.add("Hibernate ORM");
-        habilidades.add("JavaServer Pages Standard Tag Library (JSTL)");
-        habilidades.add("Apache Struts");
+        habilidades.add("Spring Framework");
         habilidades.add("Apache Maven");
         habilidades.add("JUnit Testing");
-        // Web Technologies
-        habilidades.add("HTML, CSS, and JavaScript");
-        habilidades.add("JavaServer Pages (JSP)");
-        habilidades.add("JavaServer Faces (JSF)");
         habilidades.add("Spring MVC");
-        habilidades.add("RESTful Web Services (JAX-RS)");
-        habilidades.add("SOAP Web Services (JAX-WS)");
-        // Databases
-        habilidades.add("SQL (Structured Query Language)");
-        habilidades.add("Relational Database Management Systems (RDBMS)");
+        habilidades.add("SQL");
         habilidades.add("MySQL");
-        habilidades.add("Oracle Database");
         habilidades.add("PostgreSQL");
-        // Other Tools and Technologies
         habilidades.add("Version Control Systems (e.g., Git)");
         habilidades.add("IDEs (Integrated Development Environments) like Eclipse or IntelliJ IDEA");
-        habilidades.add("Build tools like Maven");
         habilidades.add("Application servers like Apache Tomcat");
         cadastro.setHabilidades(habilidades);
 
+        cadastro.setSexo(Sexo.MASCULINO);
 
         Profissao profissao = profissaoRepository.findByNome("Analista de Sistemas");
         cadastro.setProfissao(profissao);
 
-        Set<CadastroExperiencia> cadastroExperiencias = new HashSet<>();
+        return cadastro;
+    }
+
+    private CadastroExperiencia preencherCadastroExperiencia(Cadastro cadastro){
+
+        // Set<CadastroExperiencia> cadastroExperiencias = new HashSet<>();
         CadastroExperiencia cadastroExperiencia = new CadastroExperiencia();
         cadastroExperiencia.setSalario(2500.0);
         cadastroExperiencia.setEmpregoAtual(false);
         cadastroExperiencia.setDataContratacao(LocalDate.now().minusYears(3));
         cadastroExperiencia.setDataDesligamento(LocalDate.now().minusMonths(3));
         cadastroExperiencia.setRegimeContratacao(RegimeContratacao.CLT);
-        profissao = profissaoRepository.findByNome("Analista de Testes");
+
+        // private Empresa empresa;
+        
+        Profissao profissao = profissaoRepository.findByNome("Analista de Testes");
         cadastroExperiencia.setProfissao(profissao);
-        cadastroExperiencias.add(cadastroExperiencia);
-        cadastro.setCadastroExperiencias(cadastroExperiencias);
+        cadastroExperiencia.setCadastro(cadastro);
+        // cadastroExperiencias.add(cadastroExperiencia);
+        //cadastro.setCadastroExperiencias(cadastroExperiencias);
 
-        return cadastro;
+        return cadastroExperiencia;
     }
-
 }
