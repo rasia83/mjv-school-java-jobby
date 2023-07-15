335a5d4b7a62cca91a65b52e22d5d4b4d3ff1aca
Rasia83 <tiago_rasia@hotmail.com>
2023-07-15 15:20:04 -0300

    Update ProfissaoRepository.java

--- Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/ProfissaoRepository.java
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/ProfissaoRepository.java
@@ -2,11 +2,12 @@ package com.mjvinnovation.school.jobby.repository;
 
 import com.mjvinnovation.school.jobby.model.Profissao;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ProfissaoRepository  extends JpaRepository<Profissao, Integer> {
 
     // @Query("SELECT p FROM Profissao p WHERE p.nome LIKE :nome")
     Profissao findByNome(String nome);
+
+    Profissao findByNomeIgnoreCase(String nome);
 
 }
