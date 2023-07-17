a7b3de804d2e8d52fb5a937b8ca368f1d24f931c
Rasia83 <tiago_rasia@hotmail.com>
2023-07-17 02:09:47 -0300

    Update CadastroRepository.java

--- Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/CadastroRepository.java
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/repository/CadastroRepository.java
@@ -1,7 +1,20 @@
 package com.mjvinnovation.school.jobby.repository;
 
 import com.mjvinnovation.school.jobby.model.Cadastro;
+import com.mjvinnovation.school.jobby.model.Sexo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CadastroRepository extends JpaRepository<Cadastro, Integer> {
+
+    @Query("SELECT c FROM Cadastro c JOIN Endereco e ON c.endereco = e.id JOIN Cidade ci ON " +
+            "e.cidade = ci.codigoMunicipio WHERE c.sexo = :sexo AND ci.uf = :uf")
+    List<Cadastro> findBySexoAndUf(Sexo sexo, String uf);
+
+
+    public Optional<Cadastro> findByNomeContainingIgnoreCase(String cadastro);
+
 }
